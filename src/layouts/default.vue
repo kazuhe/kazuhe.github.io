35840e64043c43bd029ebe3f3e2cc47ca3f277e6
kazuhe <omc.ohara@gmail.com>
2022-07-31 17:18:22 +0900

    Add HeaderNavigation

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="font-mono bg-zinc-100 h-screen text-sm">
+    <div class="bg-zinc-50 border-x border-zinc-200 h-screen w-[1000px] m-auto">
+      <header class="w-full">
+        <div>FOO</div>
+        <HeaderNavigation />
+      </header>
+      Some default layout shared across all pages
+      <slot />
+    </div>
+  </div>
+</template>
