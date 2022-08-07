d3690930d93be37f49f1138987e79b3ee7a2b52f
Ohara Kazuhiro <57878514+kazuhe@users.noreply.github.com>
2022-08-08 00:40:56 +0900

    Fix #25 (#26)
    * Setting meta-tag and OGP
    
    * Google Analytics
    
    * Fix

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,42 @@
+<script setup lang="ts">
+const title = "kazuhe.github.io";
+const description = "kazuhe's homepage.";
+
+useHead({
+  title,
+  htmlAttrs: {
+    lang: "ja",
+  },
+  meta: [
+    { name: "description", content: description },
+    {
+      property: "og:title",
+      content: title,
+    },
+    {
+      property: "og:description",
+      content: description,
+    },
+    {
+      property: "og:image",
+      content: "/icon.png",
+    },
+    {
+      property: "og:url",
+      content: "https://kazuhe.github.io/",
+    },
+    {
+      name: "twitter:card",
+      content: "summary",
+    },
+    {
+      name: "twitter:site",
+      content: "@kazuhe__",
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
