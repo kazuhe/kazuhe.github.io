bfb5537348aba34c22bd460f70840a46e054290f
kazuhe <omc.ohara@gmail.com>
2022-08-13 17:10:45 +0900

    Fix #49

--- src/infrastructures/activity/github/index.ts
+++ src/infrastructures/activity/github/index.ts
@@ -1,23 +1,24 @@
+import fs from "fs";
+import path from "path";
 import { Pull } from "@/domain/activity";
 
 /**
  * GitHub の Pull Request を取得する
  */
-export const fetchPulls = async (
-  owner: string,
-  repo: string,
-  number: number
-): Promise<Pull | undefined> =>
-  fetch(`https://api.github.com/repos/${owner}/${repo}/pulls/${number}`)
-    .then((reponse) => reponse.json())
-    .then((data) => ({
-      title: data.title,
-      description: data.body.substring(0, 150) + "...",
-      url: data.url,
-      createdAt: data.created_at,
-      number: data.number,
-      repo: data.base.repo.full_name,
-    }))
+export const fetchPulls = async (): Promise<Pull[] | undefined> =>
+  fs.promises
+    .readFile(path.resolve("json", "github-pulls.json"), "utf-8")
+    .then((reponse) => JSON.parse(reponse))
+    .then((data) =>
+      data.map((data) => ({
+        title: data.title,
+        description: data.body.substring(0, 150) + "...",
+        url: data.html_url,
+        createdAt: data.created_at,
+        number: data.number,
+        repo: data.base.repo.full_name,
+      }))
+    )
     .catch((err) => {
       console.error(err);
       return undefined;
