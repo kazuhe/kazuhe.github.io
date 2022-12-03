65ae87c1f7d2fb428111079f1a262d0fba95d906
kazuhe <omc.ohara@gmail.com>
2022-12-03 16:09:30 +0900

    to Nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,21 @@
-import path from "path";
-import { defineNuxtConfig } from "nuxt";
+// import path from "path";
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   srcDir: "src/",
+  modules: ["@nuxtjs/tailwindcss"],
+  // modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  
+  imports: {
+    autoImport: false
+  }
 
   /**
    * @nuxt/content
    *
    * https://content.nuxtjs.org/api/configuration
    */
-  content: {
-    sources: [path.join(__dirname, "content")],
-  },
-});
+  //  content: {
+  //   sources: [path.join(__dirname, "content")],
+  // },
+})
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,51 +1,52 @@
 <script setup lang="ts">
-import Parser from "rss-parser";
-import { Blog, sortBlogs } from "@/domain/blog";
-import BlogCard from "@/components/BlogCard.vue";
+// import Parser from "rss-parser";
+// import { Blog, sortBlogs } from "@/domain/blog";
+// import BlogCard from "@/components/BlogCard.vue";
 
 /**
  * zenn の RSS からコンテンツを取得する
  */
-const fetchZennContent = (): Promise<Blog[]> =>
-  Promise.resolve(
-    new Parser().parseURL("https://zenn.dev/kazuhe/feed?all=1").then((res) =>
-      res.items.map((d) => ({
-        title: d.title,
-        description: d.content,
-        path: d.link,
-        createdAt: d.pubDate,
-        icon: "z",
-        type: "zenn",
-      }))
-    )
-  );
+// const fetchZennContent = (): Promise<Blog[]> =>
+//   Promise.resolve(
+//     new Parser().parseURL("https://zenn.dev/kazuhe/feed?all=1").then((res) =>
+//       res.items.map((d) => ({
+//         title: d.title,
+//         description: d.content,
+//         path: d.link,
+//         createdAt: d.pubDate,
+//         icon: "z",
+//         type: "zenn",
+//       }))
+//     )
+//   );
 
 /**
  * `/content` からコンテンツを取得する
  */
-const fetchOwnContent = (): Promise<Blog[]> =>
-  queryContent("blog")
-    .find()
-    .then((res) =>
-      res.map((d) => ({
-        title: d.title,
-        description: d.description,
-        path: d._path,
-        createdAt: d.created_at,
-        icon: d.icon,
-        type: "own",
-      }))
-    );
+// const fetchOwnContent = (): Promise<Blog[]> =>
+//   queryContent("blog")
+//     .find()
+//     .then((res) =>
+//       res.map((d) => ({
+//         title: d.title,
+//         description: d.description,
+//         path: d._path,
+//         createdAt: d.created_at,
+//         icon: d.icon,
+//         type: "own",
+//       }))
+//     );
 
-const { data: blogs } = await useAsyncData("blogs", () =>
-  Promise.all([fetchZennContent(), fetchOwnContent()]).then(sortBlogs)
-);
+// const { data: blogs } = await useAsyncData("blogs", () =>
+//   Promise.all([fetchZennContent(), fetchOwnContent()]).then(sortBlogs)
+// );
 </script>
 
 <template>
   <div>
     <ul>
-      <li v-for="blog in blogs" :key="blog.path" class="first:mt-0 mt-4">
+      <li>Foo</li>
+      <!-- <li v-for="blog in blogs" :key="blog.path" class="first:mt-0 mt-4">
         <blog-card
           :title="blog.title"
           :description="blog.description"
@@ -54,7 +55,7 @@ const { data: blogs } = await useAsyncData("blogs", () =>
           :icon="blog.icon"
           :type="blog.type"
         />
-      </li>
+      </li> -->
     </ul>
   </div>
 </template>
