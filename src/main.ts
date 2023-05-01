a314e5fe683ed5de38c7466b736fcd568db2aceb
kazuhe <omc.ohara@gmail.com>
2023-05-02 00:21:02 +0900

    Add timeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter";
-import { createTimeline } from "./createTimeline";
+import { createTimeline, convertJsonToTimeline } from "./browser/timeline";
 
 const app = document.querySelector("#app");
 
@@ -10,37 +7,11 @@ if (!app) {
   throw new Error("#app element not found");
 }
 
-const timeline = [
-  {
-    date: "2021-01-01",
-    title: "ICONIX プロセスによる Java 製 REST API の設計",
-    link: "/",
-  },
-  {
-    date: "2021-01-01",
-    title: "Vite 2.0",
-    link: "/",
-  },
-];
+(async () => {
+  const zenn = await import(`../.articles/zenn.json`);
+  const hatena = await import(`../.articles/hatena.json`);
 
-app.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+  const timeline = convertJsonToTimeline([...zenn.default, ...hatena.default]);
 
-app.appendChild(createTimeline(timeline));
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+  app.appendChild(createTimeline(timeline));
+})();
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [addDynamicIconSelectors()],
+};
